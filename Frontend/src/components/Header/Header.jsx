6c15c064e4579c9d91d6d8bb2b12e02cbcdd85a9
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-09-06 01:37:39 +0530

    Making a commit

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import './Header.css';
 
 
 export default function Header() {
     return (
-        <Header className="header">
-            Hi
-        </Header>
+        <>Hi</>
     );
 };
 

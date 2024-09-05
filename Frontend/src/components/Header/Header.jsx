d6237897f319f3ffa900cb781f0580a9cd38aed8
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-09-06 01:26:46 +0530

    An error in Header

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+    return (
+        <Header className="header">
+            Hi
+        </Header>
+    );
+};
+
+export default Header;

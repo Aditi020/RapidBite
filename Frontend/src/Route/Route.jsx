3c0253ea5b92cc750453704612d0df8c6823fbe6
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-09-11 00:29:46 +0530

    Added a Food Description Page

--- Frontend/src/Route/Route.jsx
+++ Frontend/src/Route/Route.jsx
@@ -6,6 +6,7 @@ import Checkout from "../pages/Checkout";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Registeration";
+import FoodDetails from "../pages/FoodDetails";
 
 const Routers = () => {
     return (
@@ -13,6 +14,7 @@ const Routers = () => {
             <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/home" element={<Home />} />
             <Route path="/foods" element={<AllFoods />} />
+            <Route path="/foods/:id" element={<FoodDetails />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/login" element={<Login />} />

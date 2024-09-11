eaecfe193b51f14f47b1bf8bfb9997ed1e5d2773
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-09-11 10:40:11 +0530

    Updated Tesimonials

--- Frontend/src/components/UI/Testimonial.jsx
+++ Frontend/src/components/UI/Testimonial.jsx
@@ -1,64 +1,109 @@
-import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
 
-// import ava01 from "../../../assets/images/ava-1.jpg";
-// import ava02 from "../../../assets/images/ava-2.jpg";
-// import ava03 from "../../../assets/images/ava-3.jpg";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../../styles/Testimonial.css';
 
-import "../../styles/Testimonial.css";
+export default function Testimonial() {
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
 
-const TestimonialSlider = () => {
-  const settings = {
-    dots: true,
-    autoplay: true,
-    infinite: true,
-    speed: 1000,
-    autoplaySpeed: 3000,
-    swipeToSlide: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+                autoplay={{
+                    delay: 2000
+                }}
 
-  return (
-    <Slider {...settings}>
-      <div>
-        <p className="review__text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          atque, quam minus totam maiores laborum! Impedit consectetur illum
-          aliquid odit. Odit dolore ipsum quod debitis nostrum necessitatibus
-          quis dolorem quas!
-        </p>
-        <div className="slider__content d-flex align-items-center gap-3">
-          {/* <img src={ava01} alt="avatar" className=" rounded" /> */}
-          <h6>Jhon Doe</h6>
-        </div>
-      </div>
-      <div>
-        <p className="review__text">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          atque, quam minus totam maiores laborum! Impedit consectetur illum
-          aliquid odit. Odit dolore ipsum quod debitis nostrum necessitatibus
-          quis dolorem quas!"
-        </p>
-        <div className="slider__content d-flex align-items-center gap-3">
-          {/* <img src={ava02} alt="avatar" className=" rounded" /> */}
-          <h6>Mitchell Marsh</h6>
-        </div>
-      </div>
-      <div>
-        <p className="review__text">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          atque, quam minus totam maiores laborum! Impedit consectetur illum
-          aliquid odit. Odit dolore ipsum quod debitis nostrum necessitatibus
-          quis dolorem quas!"
-        </p>
-        <div className="slider__content d-flex align-items-center gap-3">
-          {/* <img src={ava03} alt="avatar" className=" rounded" /> */}
-          <h6>Steven Crock</h6>
-        </div>
-      </div>
-    </Slider>
-  );
-};
+                modules={[Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide className='testimonial_slider_card'>
 
-export default TestimonialSlider;
+                    <div className='customer_feedback'>
+                        <p>
+                            &quot; A very evil person with a lots and lots of evil stickers. &quot;
+                        </p>
+                    </div>
+
+                    <div className='customer_detail'>
+
+                        <div className='customer_profile_picture'>
+                            <img src="https://images.pexels.com/photos/1561020/pexels-photo-1561020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        </div>
+
+                        <div className='customer_profile_picture'> <span className='customer_name'></span>Evil Wikie</div>
+
+                    </div>
+
+                </SwiperSlide>
+
+                <SwiperSlide className='testimonial_slider_card'>
+
+                    <div className='customer_feedback'>
+                        <p>
+                            &quot; A very kind and innocent person. &quot;
+                        </p>
+                    </div>
+
+                    <div className='customer_detail'>
+
+                        <div className='customer_profile_picture'>
+                            <img src="https://images.pexels.com/photos/1561020/pexels-photo-1561020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        </div>
+
+                        <div className='customer_profile_picture'> <span className='customer_name'></span>Deepansh Srivastav</div>
+
+                    </div>
+
+                </SwiperSlide>
+
+                <SwiperSlide className='testimonial_slider_card'>
+
+                    <div className='customer_feedback'>
+                        <p>
+                            &quot; A very evil person with a lots and lots of evil stickers. &quot;
+                        </p>
+                    </div>
+
+                    <div className='customer_detail'>
+
+                        <div className='customer_profile_picture'>
+                            <img src="https://images.pexels.com/photos/1561020/pexels-photo-1561020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        </div>
+
+                        <div className='customer_profile_picture'> <span className='customer_name'></span>Evil Wikie</div>
+
+                    </div>
+
+                </SwiperSlide>
+
+                <SwiperSlide className='testimonial_slider_card'>
+
+                    <div className='customer_feedback'>
+                        <p>
+                            &quot; A very kind and innocent person. &quot;
+                        </p>
+                    </div>
+
+                    <div className='customer_detail'>
+
+                        <div className='customer_profile_picture'>
+                            <img src="https://images.pexels.com/photos/1561020/pexels-photo-1561020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        </div>
+
+                        <div className='customer_profile_picture'> <span className='customer_name'></span>Deepansh Srivastav</div>
+
+                    </div>
+
+                </SwiperSlide>
+
+
+
+            </Swiper>
+        </>
+    );
+}
